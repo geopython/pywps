19cb5440873d128d5aca5b3c7349fcbc3d06ad7e
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2006-10-23 19:07:27 +0000

    added support for debian packages

---
+++ debian/pywps-default.ex
@@ -0,0 +1,10 @@
+# Defaults for pywps initscript
+# sourced by /etc/init.d/pywps
+# installed at /etc/default/pywps by the maintainer scripts
+
+#
+# This is a POSIX shell fragment
+#
+
+# Additional options that are passed to the Daemon.
+DAEMON_OPTS=""
