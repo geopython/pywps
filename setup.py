2aa3c0af880a9c89d336c7771fd3d56fb8405585
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2006-11-02 14:35:29 +0000

    rc3

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ should be as easy as possible.
 """
 
 classifiers=[
-          'Development Status :: 0.1',
+          'Development Status :: 1.0rc3',
           'Environment :: CGI',
           'Environment :: Web Environment',
           'Intended Audience :: Developers',
@@ -33,14 +33,14 @@ doclines = __doc__.split("\n")
 
 setup(
         name = 'pywps',
-        version = '0.1',
+        version = '1.0',
         maintainer="Jachym Cepicky",
         maintainer_email = 'jachym.cepicky@centrum.cz',
         author = 'Jachym Cepicky',
         author_email = 'jachym.cepicky@centrum.cz',
-        url = 'http://les-ejk.cz/?cat=pywps',
+        url = 'http://pywps.wald.intevation.de',
         license = "http://www.gnu.org/licenses/gpl.html",
-        download_url="http://les-ejk.cz/?cat=pywps",
+        download_url="http://pywps.wald.intevation.de",
         description=doclines[0],
         platforms=["any"],
         classifiers= classifiers,
