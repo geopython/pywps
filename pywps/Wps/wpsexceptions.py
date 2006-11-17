dd77649fd4a6b8c9a08075fef062687a594dee09
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2006-11-17 10:43:37 +0000

    defining new set of exception classes

--- pywps/Wps/wpsexceptions.py
+++ pywps/Wps/wpsexceptions.py
@@ -15,7 +15,6 @@ class WPSExceptions:
         self.ExceptionReport.setAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance")
         self.ExceptionReport.setAttribute("version","1.0.0")
         self.document.appendChild(self.ExceptionReport)
-
         return
 
     def make_exception(self,code,locator=None):
@@ -39,4 +38,61 @@ class WPSExceptions:
         return self.document.toprettyxml(indent='\t', newl='\n', encoding="utf-8")
 
         
+class WPSException(Exception):
+    """
+    WPSException should be base class for all exeptions
+    """
 
+    def make_xml(self):
+        # formulate XML
+        self.document = Document()
+        self.ExceptionReport = self.document.createElementNS("http://www.opengis.net/ows","ExceptionReport")
+        self.ExceptionReport.setAttribute("xmlns","http://www.opengis.net/ows")
+        self.ExceptionReport.setAttribute("xmlns:xsi","http://www.w3.org/2001/XMLSchema-instance")
+        self.ExceptionReport.setAttribute("version","1.0.0")
+        self.document.appendChild(self.ExceptionReport)
+
+        # make exception
+                
+        self.Exception = self.document.createElement("Exception")
+        self.Exception.setAttribute("exceptionCode",self.code)
+
+        if self.locator:
+            self.Exception.setAttribute("locator",self.locator)
+            self.ExceptionReport.appendChild(self.Exception)
+
+        self.ExceptionReport.appendChild(self.Exception)
+
+    def __str__(self):
+        return self.document.toprettyxml(indent='\t', newl='\n', encoding="utf-8")
+
+class MissingParameterValue(WPSException)
+    def __init__(self, value):
+        self.code = "MissingParameterValue"
+        self.locator = value
+        self.make_xml()
+
+class InvalidParameterValue(WPSException):
+    def __init__(self,value):
+        self.code = "NoApplicableCode"
+        self.locator = value
+        self.make_xml()
+
+class NoApplicableCode(WPSException):
+    def __init__(self,value=None):
+        self.code = "NoApplicableCode"
+        self.locator = value
+        self.make_xml()
+        self.ExceptionReport.appendChild(self.document.createComment(repr(locator)))
+
+class ServerBusy(WPSException):
+    def __init__(self,value=None):
+        self.code = "ServerBusy"
+        self.locator = value
+        self.make_xml()
+
+class FileSizeExceeded(WPSException):
+    def __init__(self,value=None):
+        self.code = "FileSizeExceeded"
+        self.locator = value
+        self.make_xml()
