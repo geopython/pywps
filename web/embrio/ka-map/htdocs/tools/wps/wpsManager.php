4da5cafeef562a91d77d48e187f9cecae426720a
ominiverdi <ominiverdi@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2006-11-21 18:43:11 +0000

    process output drawing over ka-map. version 002

--- web/embrio/ka-map/htdocs/tools/wps/wpsManager.php
+++ web/embrio/ka-map/htdocs/tools/wps/wpsManager.php
@@ -153,7 +153,7 @@ $img_path = '';//the path where pywps store the image file
 	$style->color->setRGB( -1,-1,-1);
 	
 	
-if($map->save($szQueryCacheDir."/mapfile_buffer.map")){
+if($oMap->save($szQueryCacheDir."/mapfile_buffer.map")){
 	
 	$oMap->savequery($szQueryCacheDir."query.bin");
 	
