54a5ff17b46283f2893541c17ccf2ec9a0d73ce7
ominiverdi <ominiverdi@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2006-11-21 18:34:18 +0000

    process output drawing over ka-map. version 002

--- web/embrio/ka-map/htdocs/tools/wps/wpsManager.php
+++ web/embrio/ka-map/htdocs/tools/wps/wpsManager.php
@@ -117,7 +117,7 @@
 }*/
 
 //PARAMS TO BE PUT OUTSIDE THIS FILE
-$cgi_executable = '/cgi-bin/wps.py';
+$cgi_executable = 'http://localhost/cgi-bin/wps.py';
 $img_path = '';//the path where pywps store the image file
 
 //Built up the request for PYWPS	
