fa44034c071d8a3fdc292b8b86c2ff68a60b82f4
ominiverdi <ominiverdi@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2006-11-23 18:20:48 +0000

    Embrio. interface revision. version 001

--- web/embrio/raster/r_los/js/startUp_kamap.js
+++ web/embrio/raster/r_los/js/startUp_kamap.js
@@ -30,23 +30,156 @@
 
 
 //MAP variables
-var outimg;
-var map_extent;
-var height_extent;
-var distance_extent;
+var outimg = null;//old stuff
+var map_extent = null;//old stuff
+var height_extent = null;//old stuff
+var distance_extent = null;//old stuff
 var input_x=null;
 var input_y=null;
 
 //ajax call vars
-var image_url;
-var xml_dump;
-
-
-
+var image_url = null;//old stuff
+var xml_dump = null;//old stuff
 
 //interface vars
-var steps = 6;
+var steps = 6; //very old stuff
 
+
+var myKaMap = null;
+var queryParams = null;
+		SET_DHTML();
+		
+		//interface vars
+		var canvas; //show appended objects
+		
+		//MODULE VARS
+		var module_path = 'mod/mod_r_los.php';
+		var identifier = 'visibility2';//Identifier name for WPS service
+		var wpsCache = '/home/doktoreas/pywps.ominiverdi.org/subversion/trunk/web/tmp/';
+				
+		
+		//WPS manager
+		var wpsManager;
+		
+		
+		/**
+		 * parse the query string sent to this window into a global array of key = value pairs
+		 * this function should only be called once
+		 */
+		function parseQueryString()
+		{
+			queryParams = [];
+			var s=window.location.search;
+			if (s!='')
+			{
+				s=s.substring( 1 );
+				var p=s.split('&');
+				for (var i=0;i<p.length;i++)
+				{
+					var q=p[i].split('=');
+					queryParams[q[0]]=q[1];
+				}
+			}
+		}
+		
+		/**
+		 * get a query value by key.  If the query string hasn't been parsed yet, parse it first.
+		 * Return an empty string if not found
+		 */
+		function getQueryParam(p)
+		{
+			if (!queryParams)
+			{
+				parseQueryString();
+			}
+			if (queryParams[p])
+				return queryParams[p];
+			else 
+				return '';
+		}
+		
+		function myOnLoad()
+		{
+			initDHTMLAPI();
+			
+			var map = getQueryParam('map');
+			var extents = getQueryParam('extents');
+			var cps = getQueryParam('cps');
+			
+			myKaMap = new kaMap( 'viewport' );
+			var kaNav = new kaNavigator( myKaMap );
+			kaNav.activate();
+			myKaZoomer = new kaZoomer(myKaMap); 
+			//myKaMap.resize();//??? maybe not needed
+			myKaMap.registerForEvent( KAMAP_MAP_INITIALIZED, null, myMapInitialized );
+			myKaMap.registerForEvent( KAMAP_SCALE_CHANGED, null, myScaleChanged );
+			myKaMap.registerForEvent( KAMAP_MAP_CLICKED, null, myMapClicked );
+			//myKaMap.registerForEvent( WPS_LAYER_PAINTING, null, waitEnd );
+
+			myKaMap.initialize( map, extents, cps );
+		}
+		function myMapInitialized(){
+			/* Embrio startup*/
+				//set img object
+				outimg = document.getElementById('outimg');
+				//outimg.onmousedown = getCoords;
+				//get params
+				map_extent = document.getElementById('map_extent').value;
+				map_extent = map_extent.split(',');
+				height_extent = document.getElementById('height_extent').value.split(',');
+				height_unit = parseInt((height_extent[1]-height_extent[0])/steps*100)/100;
+				distance_extent = document.getElementById('distance_extent').value.split(',');
+				distance_unit = parseInt((distance_extent[1]-distance_extent[0])/steps*100)/100;
+				
+				//print ranges
+				getRawObject('observer_range').innerHTML = height_extent[0] + ' -&gt; ' + height_extent[1]; 
+				getRawObject('maxdist_range').innerHTML = distance_extent[0] + ' -&gt; ' + distance_extent[1]; 
+				
+				//update selects
+				var maxdist = getRawObject('maxdist');
+				var observer = getRawObject('observer');
+				var height = parseInt(height_extent[0]);
+				var distance = parseInt(distance_extent[0]);
+				for(i=0;i<=steps;i++) {
+					if(i==steps){
+						height = parseInt(height_extent[1]);
+						distance = parseInt(distance_extent[1]);
+						maxdist[i] = new Option(distance,distance,false,false);
+						observer[i] = new Option(height,height,false,false);
+					} else{  
+						maxdist[i] = new Option(distance,distance,false,false);
+						observer[i] = new Option(height,height,false,false);
+					}
+					
+					height = parseInt((height + height_unit)*100)/100;
+					distance = parseInt(distance + distance_unit);
+				}
+				
+				//set button event
+				getRawObject('go').onclick = runPywps;
+				
+				//create drawing canvas
+				canvas = myKaMap.createDrawingCanvas('10');
+		}
+		function myScaleChanged(eventID, scale){
+		}
+		function myMapClicked(eventID, coords){
+			//alert(coords[0]+ ' ' + coords[1]);
+			setFields(coords[0],coords[1]);
+			
+			//empty pins
+			myKaMap.removeAllObjects(canvas);
+			//create the pin obj
+			var pinDiv = document.createElement('div');
+			pinDiv.id = 'pinDiv';
+			var pinImg = document.createElement('img');
+			pinImg.src = '../../ka-map/htdocs/images/tip-yellow.png';
+			pinImg.style.position = 'absolute';
+			pinImg.style.top = '-19px';
+			pinImg.style.left = '-6px';
+			pinDiv.appendChild(pinImg);
+			myKaMap.addObjectGeo( canvas, coords[0], coords[1], pinDiv );
+		}
 
 function runPywps(){
 	if(input_x==null){
