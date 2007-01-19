7380ce11c1c2e2818433ba61368b57c6080f0ca9
doktoreas <doktoreas@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2007-01-19 09:05:55 +0000

    add the mod file for treeno process

--- web/embrio/raster/treeno/mod/mod_treeno.php
+++ web/embrio/raster/treeno/mod/mod_treeno.php
@@ -1,7 +1,7 @@
 <?php  
  /**********************************************************************
  *
- * purpose: a connector r.los GRASS function
+ * purpose: a raster analysis process
  *
  * authors: Luca Casagrande (luca.casagrande@gmail.com) and Lorenzo Becchi (lorenzo@ominiverdi.com)
  *
@@ -30,47 +30,76 @@
  
 	include('../include/config.php');
  
+//Initialize some var for the output parsing
+	$value=0;
+	$output=array();	
         $map = ms_newMapObj($map_path.$map_file);
 	
 //Built up the request for PYWPS	
 	
-	$stringa_query = $cgi_executable."?service=wps&version=0.4.0&request=Execute&Identifier=visibility2&";
-	$array = array('x', $_REQUEST['xvalue'],'y',$_REQUEST['yvalue'],'maxdist',$_REQUEST['maxdist'],'observer',$_REQUEST['observer']);
+	$stringa_query = $cgi_executable."?service=wps&version=0.4.0&request=Execute&Identifier=trees&";
+	$array = array('input', $_REQUEST['input']);
 	$comma_separated = implode(",", $array);
 	$stringa_query .= "datainputs=";
 	$stringa_query .= $comma_separated;
-	$stringa_query .= "&status=true&store=true";
+	$stringa_query .= "&status=false&store=true";
 	
-//Extract the file name.This part need more debugging
-
 	$dom = new DOMDocument();
 	$dom->load($stringa_query);
-	$CVR = $dom->getElementsByTagName('ComplexValueReference');
+	
+//Output are 2 vector files (one area and one point) and 1 costant
+
+//Extract the path of the vector files
+
+foreach ($dom->getElementsByTagName('ComplexValueReference') as $CVR) {
+        $nodo = $CVR->getAttribute('reference');
+        $aNodo = explode('/',$nodo);
+        $filename = end($aNodo);
+        $output[$value]=$pywps_path.$filename;
+        $value++;
+        }
+
+//Extract the value of the constant
+	$trees=$dom->getElementsByTagName('LiteralValue');
+		
+
+
+/*	$CVR = $dom->getElementsByTagName('ComplexValueReference');
 	$nodo = $CVR->item(0)->getAttribute('reference');
 	$aNodo = explode('/',$nodo);
 	$filename = end($aNodo);
 	$pywps_outputPath.=$filename;
-	
-//Update the mapfile with new layer
+*/	
 
+//Update the mapfile with first layer voronoi (area)
+	 
 	$layer = ms_newLayerObj($map);
-        $layer->set('name', "visibility");
-	$layer->set('status', MS_DEFAULT );
-	$layer->set('data', $img_path.$filename);
-	$layer->set('type', MS_LAYER_RASTER);
-    
-	
-		$map->imagecolor->setRGB( -1,-1,-1);
-
+   	$layer->set('name', "voronoi");
+   	$layer->set('status', MS_DEFAULT );
+   	$layer->set('connection', $output[0]);
+   	$layer->set('type', MS_LAYER_POLYGON);
+   	$layer->set('transparency',"50");
+   	$layer->set('connectiontype',MS_OGR);
         $class = ms_newClassObj($layer);
-        $class->setExpression("1");
-	$style = ms_newStyleObj($class);
+        $style = ms_newStyleObj($class);
         $style=$class->getStyle(0);
-        $style->color->setRGB( -1,-1,-1);
+        $style->color->setRGB( 255,255,0);
+
+//This is for the layer top (point)
+
+	$layer= ms_newLayerObj($map);
+   	$layer->set('name', "top");
+   	$layer->set('status', MS_DEFAULT );
+   	$layer->set('connection', $output[1]);
+   	$layer->set('type', MS_LAYER_POINT);
+   	$layer->set('connectiontype',MS_OGR);
+        $class= ms_newClassObj($layer);
+        $style= ms_newStyleObj($class);
+        $style=$class->getStyle(0);
+        $style->color->setRGB( 255,0,0);
 		
 		
-		
-	$map->save($img_path."/mapfile_buffer.map");
+	$map->save($img_path."/mapfile_tree.map");
 //Create the output image
 
 	$map_id = sprintf("%0.6d",rand(0,999999));
@@ -82,10 +111,10 @@
 	echo "image_url='$image_url';";
 	//echo "xml_dump='".urlencode($dom->saveXML())."';";
 	echo "mapfile='/tmp/".$sessionId.".map';";
-	//echo "mapfile='$img_path."/mapfile_buffer.map"
+	//echo "mapfile='$img_path."/mapfile_tree.map"
 	if(isset($_REQUEST['debug'])){
-			echo "<br>filename = $filename";
-			echo "<br>outimg = $image_url";
-			echo "<br>".$img_path."/mapfile_buffer.map";
+	echo "<br>filename = $filename";
+	echo "<br>outimg = $image_url";
+	echo "<br>".$img_path."/mapfile_treeno.map";
 	}
 ?>	
