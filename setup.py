944bf0871b39a16a121e1c363e5a56ae28d9f37f
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2007-09-17 16:35:29 +0000

    rc3 prepared

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ doclines = __doc__.split("\n")
 
 setup(
         name = 'pywps',
-        version = '2.0.0-rc1',
+        version = '2.0.0-rc3',
         maintainer="Jachym Cepicky",
         maintainer_email = 'jachym@les-ejk.cz',
         author = 'Jachym Cepicky',
