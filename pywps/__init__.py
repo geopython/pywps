5d4feb00ab8a28446cde47950cb81bc696c3705f
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2008-07-18 08:32:11 +0000

    updated documentation for pywps/__init__.py

--- pywps/__init__.py
+++ pywps/__init__.py
@@ -1,1 +1,12 @@
+"""PyWPS main package.
+
+This package contains classes necessary for input parsing and request
+performing. The output is done via python's htmltmpl templating system.
+
+Here is also the default package with WPS Processes.
+
+$Id$
+"""
+
+
 __all__ = [ "Parser","processes", "Exceptions", "WPS", "Templates", "Lang"]
