7aedf179f3f32db3bac934d1fc4cca856302a5e6
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2008-09-16 11:56:15 +0000

    LOS process upgraded to PyWPS 3.0.0

--- pywps/processes/exampleLosProcess.py
+++ pywps/processes/exampleLosProcess.py
@@ -1,147 +1,79 @@
-import sys, os
+"""
+Example Linies of sight process (visibility analysis. Needs temporary
+GRASS location.
 
-from pywps.Wps.process import GRASSWPSProcess
+Author: jachym
+Licence: GNU/GPL
 
-class Process(GRASSWPSProcess):
+Changes: 
+    2008-09-16
+        Version 0.2
+        updated for PyWPS 3.0.0, czech translation
+"""
+
+from pywps.Process.Process import WPSProcess
+
+
+class Process(WPSProcess):
     def __init__(self):
-        GRASSWPSProcess.__init__(self,
-                Identifier = "los",
-                Title= "Lines of sight",
-                processVersion = "0.1",
+        WPSProcess.__init__(self,
+                identifier = "exampleLosProcess",
+                title= "Lines of sight",
+                abstract = """Visibility analysis on input digital elevation model""",
+                version = "0.2",
                 storeSupported = True,
-                statusSupported = True)
+                statusSupported = True,
+                grassLocation = True)
 
-        self.AddComplexInput(Identifier = "dmt",
-                            Title = "Digital elevation model",
-                            Formats= ["image/tiff"])
+        self.dem = self.addComplexInput(identifier = "dem",
+                            title = "Digital elevation model",
+                            abstract = """Input digital elevation model in
+                            GeoTIFF format""",
+                            formats= [{"mimeType":"image/tiff"}])
                              
-        self.AddLiteralInput(Identifier = "x",
-                            Title = "x coordinate",
-                            value = ["*"],
+        self.easting = self.addLiteralInput(identifier = "easting",
+                            title = "Easting coordinate",
+                            type = type(0.0))
+
+        self.northing = self.addLiteralInput(identifier = "northing",
+                            title = "Northing coordinate",
                             type = type(0.0))
                 
-        self.AddLiteralInput(Identifier = "y",
-                            Title = "y coordinate",
-                            value = ["*"],
-                            type = type(0.0))
-
-        self.AddLiteralInput(Identifier = "extent",
-                            Title = "Extent",
-                            Abstract = "w s e n",
-                            type = type(""),
-                            value = "")
-
-        self.AddLiteralInput(Identifier = "height",
-                            Title = "Height",
-                            Abstract = "Observer height",
+        self.height = self.addLiteralInput(identifier = "height",
+                            title = "Observer height",
                             type = type(0.0),
-                            value = 0)
+                            allowedValues = [[0,50]],
+                            default = 1.3,
+                            minOccurs=0)
 
-        self.AddLiteralInput(Identifier = "min",
-                            Title = "Min level",
-                            Abstract = "Min level in dem",
-                            type = type(0.0),
-                            value = 0)
-
-        self.AddLiteralInput(Identifier = "max",
-                            Title = "Max level",
-                            Abstract = "Max level in dem",
-                            type = type(0.0),
-                            value = 0)
-
-        self.AddComplexReferenceOutput(Identifier = "map",
-                            Title = "Resulting output map",
-                            Formats = ["image/gif"])
-        self.AddLiteralOutput(Identifier = "size",
-                            Title = "Image size")
-        self.AddLiteralOutput(Identifier = "extent",
-                            Title = "Image extent")
+        self.los = self.addComplexOutput(identifier = "los",
+                            title = "Resulting output map",
+                            formats = [
+                                {"mimeType":"image/tiff"},
+                                {"mimeType":"image/png"}
+                            ])
 
     def execute(self):
 
-        w,s,e,n = [None, None, None, None]
-        dmt = self.GetInputValue("dmt")
-        height = self.GetInputValue("height");
+        self.cmd("r.in.gdal -o input=%s output=dem" % (self.dem.value))
+        self.cmd("g.region rast=dem")
 
-        min = self.GetInputValue("min");
-        max = self.GetInputValue("max");
+        self.status.set(msg="Calculating  lines of sigth", percentDone=20)
+        self.cmd("r.los in=dem out=los coor=%f,%f obs_e=%s >&2" % \
+                (self.easting.value,self.northing.value,self.height.value))
 
-        # import of the data
-        if (self.GetInputValue("extent") != ""):
-            w,s,e,n = self.GetInputValue("extent").split(" ")
-            try:
-                w = float(w)
-                s = float(s)
-                e = float(e)
-                n = float(n)
-            except Exception, e:
-                return e
+        self.status.set(msg="Making transparency", percentDone=70)
+        self.cmd("r.null map=los setnull=0")
 
-            self.GCmd("gdal_translate -ot Byte -a_ullr %s %s %s %s -of GTiff %s dmt.tif" % (w,n,e,s,dmt))
-            dmt = "dmt.tif"
+        self.status.set(msg="Exporting resulting image", percentDone=80)
 
-        if float(height) >0:
-            #height = float(256)/float(3000)*float(height)
-            pass
+        # deside, if the user wants PNG or GeoTIFF format
+        losfile = "los.png"
+        if self.los.format["mimeType"] == "image/tiff":
+            losfile = "los.tif"
+            self.cmd("r.out.gdal in=los out=los.tif type=Byte")
         else:
-            height = 0
-        self.GCmd("r.in.gdal -o input=%s output=dmt" % (dmt))
-        self.GCmd("g.region rast=dmt")
-        if min != max:
-            if min == 0:
-                min = 1
-            if min < max:
-                self.GCmd("r.mapcalc dmt=(%s-%s)/dmt+%s" % \
-                        (float(max),float(min),float(min)))
-            else:
-                self.GCmd("r.mapcalc dmt=(%s-%s)/dmt+%s" % \
-                        (float(min),float(max),float(min)))
+            self.cmd("r.out.png in=los out=los.png")
 
-
-        self.SetStatus(message="Data  imported", percent=10)
-
-        self.SetStatus(message="Calculating  lines of sigth", percent=20)
-        # adding the value
-        max = abs(float(e)-float(w))
-        if max > 5000:
-            max = 5000
-        self.GCmd("r.los in=dmt out=los coor=%f,%f obs_e=%s max=%s >&2" % \
-                (self.GetInputValue('x'),self.GetInputValue("y"),height,max))
-
-        self.SetStatus(message="Making transparency", percent=70)
-        self.GCmd("r.null map=los setnull=0")
-
-        self.SetStatus(message="Exporting resulting image", percent=80)
-        self.GCmd("r.out.png in=los out=los.png")
-
-        self.SetStatus(message="Creating png image file", percent=90)
-        #self.GCmd("export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/home/cepicky/lib; /home/cepicky/bin/convert los.png -alpha reset -transparent white los.png")
-        self.GCmd("convert los.png -transparent white los.png")
-        self.SetOutputValue("map","los.png")
-        self.SetStatus(message="Final region settings", percent=100)
-
-        n = None
-        s = None
-        w = None
-        e = None
-        rows = 0
-        cols = 0
-
-        for line in self.GCmd("g.region -g"):
-            key,val=line.strip().split("=")
-            if key == "n":
-                n=val
-            elif key == "s":
-                s=val
-            elif key == "w":
-                w=val
-            elif key == "e":
-                e=val
-            elif key == "rows":
-                rows=val
-            elif key == "cols":
-                cols=val
-
-        self.SetOutputValue("size","%s %s" % (cols, rows))
-        self.SetOutputValue("extent","%s %s %s %s" % (w,s,e,n))
+        self.los.setValue(losfile)
 
