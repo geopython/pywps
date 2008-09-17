1a12ea21431b7f618e983aad66b6f02f30bae04d
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2008-09-17 07:22:15 +0000

    fixed missing import of htmltmpl TemplateError

--- pywps/Wps/DescribeProcess.py
+++ pywps/Wps/DescribeProcess.py
@@ -24,6 +24,7 @@ WPS DescribeProcess request handler
 # Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 from Response import Response
+from htmltmpl import TemplateError
 import os
 
 class DescribeProcess(Response):
