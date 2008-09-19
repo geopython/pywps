09cd713fceccf7e77066d8785863dd5ec7a0e047
pana <pana@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2008-09-19 10:43:51 +0000

    Fixed Execute response to conform more accurately to WPS 1.0.0 standard: storeExecuteResponse, status and rawDataOutput are checked more thoroughly and exceptions may be raised in certain cases. The server performs asynchronous request when storeExecuteResponse is true (not when status is true). When status is also true, the server will update the response document with status updates, while the process is running. Also after the process is accepted, when an Exception occurs, the server will correctly fill the response document with statusFailed tag.

--- wps.py
+++ wps.py
@@ -185,21 +185,18 @@ class WPS:
         """Performs the desired WSP Request."""
 
         # the modules are imported first, when the request type is known
-        try:
-            if self.inputs["request"]  == "getcapabilities":
-                from pywps.Wps.GetCapabilities import GetCapabilities
-                self.request = GetCapabilities(self)
-            elif self.inputs["request"]  == "describeprocess":
-                from pywps.Wps.DescribeProcess import DescribeProcess
-                self.request = DescribeProcess(self)
-            elif self.inputs["request"]  == "execute":
-                from pywps.Wps.Execute import Execute
-                self.request = Execute(self)
-            else:
-                raise self.exceptions.InvalidParameterValue(
-                        "request: "+self.inputs["request"])
-        except KeyError,e:
-            raise self.exceptions.NoApplicableCode(e.message)
+        if self.inputs["request"]  == "getcapabilities":
+            from pywps.Wps.GetCapabilities import GetCapabilities
+            self.request = GetCapabilities(self)
+        elif self.inputs["request"]  == "describeprocess":
+            from pywps.Wps.DescribeProcess import DescribeProcess
+            self.request = DescribeProcess(self)
+        elif self.inputs["request"]  == "execute":
+            from pywps.Wps.Execute import Execute
+            self.request = Execute(self)
+        else:
+            raise self.exceptions.InvalidParameterValue(
+                    "request: "+self.inputs["request"])
 
     def getConfigValue(self,*args):
         """Return desired value from the configuration files
