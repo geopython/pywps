e184876133d085884224dadc899a43496e408ceb
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2008-10-01 09:27:19 +0000

    merged revision 661 to 662: * moved example processes do doc directory * updated setup.py

---
+++ pywps/processes/__init__.py
@@ -0,0 +1,6 @@
+"""Example process, which can be used with PyWPS"""
+__all__ = []
+# __all__ = ["exampleBufferProcess", "exampleXSLTProcess",
+#            "exampleBufferNoInputsProcess",
+#            "exampleLosProcess"]
+
