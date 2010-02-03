aa37920d208c1296379266d443c46b8fdef864e5
jachym <jachym@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2010-02-03 07:14:04 +0000

    added also complexRaster test process (for tests/)

--- tests/processes/moreInOne.py
+++ tests/processes/moreInOne.py
@@ -18,5 +18,16 @@ class FirstProcess(WPSProcess):
 
 class SecondProcess(WPSProcess):
     def __init__(self):
-        WPSProcess.__init__(self,identifier="second_process",
+        WPSProcess.__init__(self,identifier="complexRaster",
                             title="Second Process")
+
+        self.indata =
+        self.addComplexInput(identifier="indata",title="Complex in",
+                formats=[{"mimeType":"image/tiff"}])
+        self.outdata = self.addComplexOutput(identifier="outdata",
+                title="Compex out",
+                formats=[{"mimeType":"image/tiff"}])
+                )
+
+    def execute(self):
+        self.outdata.setValue(self.indata.getValue())
