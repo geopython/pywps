ea1b6a2b7a3ff6d23430cff0f2bffe88a107dcdf
jesus <jesus@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2010-08-10 11:16:59 +0000

    SOAP 1.1 and 1.2 support, changes in SOAP parsing

--- pywps/Soap.py
+++ pywps/Soap.py
@@ -26,10 +26,18 @@ SOAP wrapper
 
 from xml.dom import minidom
 
+from xml.sax.saxutils import unescape # Very practical unescape char converted
+from xml.xpath.Context  import Context #Context setting for namespace support
+from xml.xpath import Evaluate
+from xml.xpath import Compile
+
+#For soap 1.2 -->http://www.w3.org/2003/05/soap-envelope (self.nsIndex=0)
+#For soap 1.1 -->http://schemas.xmlsoap.org/soap/envelope/ (self.nsIndex=1)
 soap_env_NS = ["http://www.w3.org/2003/05/soap-envelope","http://schemas.xmlsoap.org/soap/envelope/"]
 soap_enc_NS = ["http://www.w3.org/2003/05/soap-encoding","http://schemas.xmlsoap.org/soap/encoding/"]
 
-SOAP_ENVELOPE="""<?xml version="1.0" encoding="UTF-8"?>
+#Envelope for soap 1.2
+SOAP_ENVELOPE12="""<?xml version="1.0" encoding="UTF-8"?>
 <soap:Envelope xmlns:soap="http://www.w3.org/2003/05/soap-envelope"
 	xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
 	xsi:schemaLocation="http://www.w3.org/2003/05/soap-envelope
@@ -37,9 +45,17 @@ SOAP_ENVELOPE="""<?xml version="1.0" encoding="UTF-8"?>
 	<soap:Body>$SOAPBODY$</soap:Body>
 </soap:Envelope>"""
 
+#Envelope for soap 1.1
+SOAP_ENVELOPE11="""<?xml version="1.0" encoding="UTF-8"?>
+<SOAP-ENV:Envelope SOAP-ENV:encodingStyle="http://schemas.xmlsoap.org/soap/encoding/" xmlns:SOAP-ENC="http://schemas.xmlsoap.org/soap/encoding/" xmlns:xsi="http://www.w3.org/1999/XMLSchema-instance" xmlns:SOAP-ENV="http://schemas.xmlsoap.org/soap/envelope/" xmlns:xsd="http://www.w3.org/1999/XMLSchema">
+<SOAP-ENV:Body>$SOAPBODY$</SOAP-ENV:Body></SOAP-ENV:Envelope>"""
+
+WPSNamespace= {"wps":"http://www.opengis.net/wps/1.0.0"}
+
+
 soap = False
 
-def isSoap(document):
+def isSoap(document): 
     global soap
     if document.localName == "Envelope" and\
             document.namespaceURI in soap_env_NS:
@@ -65,23 +81,54 @@ class SOAP:
 
         if document:
             if type(input) == type(""):
-                self.document = minidom.parseString(document)
+                self.document = minidom.parseString(unescape(document,entities={"&quot;":"'"}))
             else:
-                self.document = document
+                self.document = minidom.parseString(unescape(document.toxml(),entities={"&quot;":"'"})) # Not very efficient, the XML is converted to string and then back again to XML
             self.nsIndex = soap_env_NS.index(document.namespaceURI)
+            
+            if (self.nsIndex==1):
+            	self.soapVersion=11
+            else:
+            	self.soapVersion=12
+    #getNode has been replaced with getWPSContent()  
+    #def getNode(self,namespace,nodeName):
+    #    """Get XML nod from DOM of specified name and namespace"""
 
-    def getNode(self,namespace,nodeName):
-        """Get XML nod from DOM of specified name and namespace"""
+    #    elements = self.document.getElementsByTagNameNS(namespace, nodeName)
+    #    if len(elements) > 0:
+    #        return elements[0]
+    #    else:
+    #        return None
+    
+    def getWPSContent(self):
+    	    """Get the specific XWPS ML content of inside the SOAP request. The Element position may change if there is a SOAP header or if is was sent as a message inside the Body content"""   
+	    #Creating context that will be used by xpath
+	    context=Context(self.document)
+	    #setting WPS name spaces
+	    context.setNamespaces(WPSNamespace)
+	    #Generating xpath expression for the 3 WPS possibilities
+	    xpathExpression = Compile("//wps:Execute | //wps:GetCapabilities | //wps:DescribeProcess")
+	    #Getting WPS content as first result of list
 
-        elements = self.document.getElementsByTagNameNS(namespace, nodeName)
-        if len(elements) > 0:
-            return elements[0]
-        else:
-            return None
+	    WPSDocument=xpathExpression.evaluate(context)[0]
+	    return WPSDocument
 
-    def getResponse(self,document):
+
+    def getSOAPVersion(self):
+		return self.soapVersion
+
+    def getResponse(self,document,soapVersion):
         """Wrap document into soap envelope"""
         # very primitive, but works
+        #SOAP 1.1 Content-type: text/xml
+        # SOAP 1.2   Content-Type: application/xml maybe application/soap ?!
+        
         document = document.__str__().replace("<?xml version=\"1.0\" encoding=\"utf-8\"?>","")
-        return SOAP_ENVELOPE.replace("$SOAPBODY$",document)
-
+        #return SOAP_ENVELOPE.replace("$SOAPBODY$",document)
+       
+        if (int(soapVersion)==11):
+         	return SOAP_ENVELOPE11.replace("$SOAPBODY$",document)
+        else:
+        	return SOAP_ENVELOPE12.replace("$SOAPBODY$",document)
+        
+	
