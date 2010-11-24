f14de386a68f1c636d4138c31e8e4cacd55bcbe6
jesus <jesus@044d2fc5-fd1f-0410-945a-bc253e72fed1>
2010-11-24 14:36:01 +0000

    FEATURE. rfc4646 support as defined by WPS (xml:lang="en-CA")

--- pywps/Process/Lang.py
+++ pywps/Process/Lang.py
@@ -13,8 +13,8 @@ In the process:
     in preset language (given by client request)
 
 """
-# Author:	Jachym Cepicky
-#        	http://les-ejk.cz
+# Author:    Jachym Cepicky
+#            http://les-ejk.cz
 # Lince:
 #
 # Web Processing Service implementation
@@ -43,29 +43,35 @@ class Lang:
     # taken from
     # http://www.loc.gov/standards/iso639-2/php/code_list.php
 
-    # self.codes[0] = ISO 639-2
+   
+    #  self.codes[0] =  RFC 4646
     # self.codes[1] = ISO 639-1
     # self.codes[2] = English name
+    
+    #Good code list RFC 4646:http://sharpertutorials.com/list-of-culture-codes/
+    #Note: Previous versions supported  # self.codes[0] = ISO 639-2
+    
     codes = [
-            ["eng","en","english"],
-            ["ger","de","german"],
-            ["fre","fr","french"],
-            ["cze","cz","czech"],
-            ["ita","it","italian"],
-            ["gre","el","greek"],
-            ["cat","ca","catalan"],
-            ["spa","es","spanish"],
-            ["fin","fi","finnish"],
-            ["swe","sv","swedish"],
+            ["en-CA","en","english"],
+            ["de-DE","de","german"],
+            ["fr-FR","fr","french"],
+            ["cz-CZ","cz","czech"],
+            ["it-IT","it","italian"],
+            ["gr-GR","el","greek"],
+            ["ca-ES","ca","catalan"],
+            ["es-ES","es","spanish"],
+            ["fi-FI","fi","finnish"],
+            ["sv-SE","sv","swedish"],
+            ["pt-PT","pt","portuguese"],
             # to be continued ...
     ]
-    defaultCode ="eng"
+    defaultCode ="en-CA"
 
     # static method
     def getCode(langString):
-
+       
         for lang in Lang.codes:
-            if langString in lang:
+            if langString in [code.lower() for code in lang]:
                 return lang[0]
 
         # return None if nothing found
