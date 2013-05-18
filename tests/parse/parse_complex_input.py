fd12a6af1aaae7d19ae25201fc9235afebf16a11
Jachym Cepicky <jachym.cepicky@gmail.com>
2013-05-19 00:22:46 +0200

    adding complex input test - only prelimenar version

--- tests/parse/parse_complex_input.py
+++ tests/parse/parse_complex_input.py
@@ -19,13 +19,34 @@ class ParseComplexInputTestCase(unittest.TestCase):
     def setUp(self):
         self.inpt = ComplexInput("")
 
-    def test_parse_complex_input_GET(self):
-        """Parse complex input KVP"""
+    def test_parse_complex_input_GET_reference(self):
+        """Parse complex input KVP reference"""
+
+        url = "InputLayer=http://foo/bar.tif@schema=http://foo/xsd@encoding=utf-8@mimetype=image/tiff"
+        self.inpt.set_from_url(url)
 
         pass
 
-    def test_parse_complex_input_POST(self):
-        """Parse complex input XML"""
+    def test_parse_complex_input_GET_reference_href(self):
+        """Parse complex input KVP reference with href attribut"""
+
+        url = "InputLayer=@schema=http://foo/xsd@encoding=utf-8@mimetype=image/tiff@href=http://foo/bar.tif"
+        self.inpt.set_from_url(url)
+
+        pass
+
+    def test_parse_complex_input_POST_reference(self):
+        """Parse complex input XML reference"""
+        
+        strin = StringIO("""<wps:Input>
+			<ows:Identifier>InputLayer</ows:Identifier>
+			<ows:Title>The layer which's values shall be reclassified</ows:Title>
+			<wps:Reference xlink:href="http://orchestra.pisa.intecs.it/geoserver/test/height.tif" method="GET"/>
+		</wps:Input>""")
+
+        req = objectify.parse(strin)
+        self.inpt.set_from_xml(req.getroot())
+
 
         pass
 
