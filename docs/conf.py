aa788f7980c377819eeb0f8e1935da4e176d9cc2
Alex Morega <alex@grep.ro>
2013-10-15 23:20:16 +0300

    mock the lxml module
    http://read-the-docs.readthedocs.org/en/latest/faq.html#i-get-import-errors-on-libraries-that-depend-on-c-modules

--- docs/conf.py
+++ docs/conf.py
@@ -16,3 +16,22 @@ pygments_style = 'sphinx'
 
 html_theme = 'default'
 htmlhelp_basename = 'PyWPSdoc'
+
+
+class Mock(object):
+    def __init__(self, *args, **kwargs):
+        pass
+    def __call__(self, *args, **kwargs):
+        return Mock()
+    @classmethod
+    def __getattr__(cls, name):
+        if name in ('__file__', '__path__'):
+            return '/dev/null'
+        elif name[0] == name[0].upper():
+            return Mock
+        else:
+            return Mock()
+
+MOCK_MODULES = ['lxml', 'lxml.etree', 'lxml.builder']
+for mod_name in MOCK_MODULES:
+    sys.modules[mod_name] = Mock()
