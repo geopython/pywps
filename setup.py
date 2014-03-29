c376002d78a29e2da007489796efbdb9222a0c9a
Jachym Cepicky <jachym.cepicky@gmail.com>
2014-03-29 08:04:44 +0100

    Fixing setup.py

--- setup.py
+++ setup.py
@@ -11,9 +11,35 @@ served back to the client. Providing GRASS funktionality on the Internet
 should be as easy as possible.
 """
 
-#   SETUP
+import sys
+import os
+import pywps
+from pywps.Template import TemplateProcessor
+
+def get_current_path():
+    return os.path.abspath(os.path.dirname(__file__))
+
+def get_templates_path(base):
+    return os.path.join(base, 'pywps', 'Templates')
+
+def compile_templates(base):
+    versionDirs = ['1_0_0']
+
+    template_files = ['GetCapabilities', 'DescribeProcess','Execute']
+
+    for version in versionDirs:
+        for template_file in template_files:
+            print "Compiling template "+template_file+" in "+base
+            template_file = os.path.join(base,version,template_file + '.tmpl')
+            template = TemplateProcessor(fileName=template_file,compile=True)
+
+# First compile templates
+PYWPS_PATH=get_current_path()
+TEMPLATES_PATH=get_templates_path(PYWPS_PATH)
+compile_templates(TEMPLATES_PATH)
+
+
 name = 'pywps'
-#   END of SETUP
 
 classifiers=[
            'Development Status :: 5 - Production/Stable',
@@ -58,12 +84,11 @@ dist =  setup(
             'pywps.Wps',
             'pywps.Wps.Execute',
             'pywps.XSLT',
-            'pywps.etc',
             'pywps.Parser',
             'pywps.Process',
+            'pywps.processes',
             'pywps.Templates',
             'pywps.Templates.1_0_0',
-            'pywps.Templates.1_0_0.inc',
         ],
         package_dir={
                     'pywps':"pywps",
@@ -78,57 +103,10 @@ dist =  setup(
                 },
         package_data={
                     'pywps':
-                    ['Templates/1_0_0/*.tmpl',
+                    ['Templates/1_0_0/*.tmplc',
                      'XSLT/*.xsl',
                      'processes/*.py-dist','processes/README',
-                     'default.cfg'],
-                    'pywps.Templates.1_0_0': ['inc/*.tmpl']
+                     'default.cfg']
                 },
         scripts=['wps.py']
 )
-
-# check, if we are really installing
-dryRun = False
-install = False
-for arg in sys.argv:
-    if arg == "--dry-run":
-        dryRun = True
-    if arg == "install":
-        install = True
-
-# post installation part
-if not dryRun and install and not os.environ.has_key("FAKEROOTKEY"):
-    # compile templates
-    # compiling before installing is necessary, because the apache
-    # webserver has not the permission to create new files in the 
-    # python site-packages directory
-
-    #setup.py can't load pywps automatically, the egg path needs to be added to 
-    #python path
-
-    #prevent loading from current path
-    sys.path=sys.path[1:]
-
-    eggPath=os.path.join(dist.command_obj['install'].install_lib,os.path.basename(dist.command_obj['bdist_egg'].egg_output))
-    sys.path.append(eggPath)
-    baseDir=os.path.join(eggPath,"pywps","Templates")
-    try:
-        from pywps.Template import TemplateProcessor
-        versionDirs = ['1_0_0']
-
-        template_files = ['GetCapabilities', 'DescribeProcess','Execute']
-
-        for version in versionDirs:
-            for template_file in template_files:
-                print "Compiling template "+template_file+" in "+baseDir
-                template_file = os.path.join(baseDir,version,template_file + '.tmpl')
-                print template_file
-                template = TemplateProcessor(fileName=template_file,compile=True)
-    except Exception,e:
-        print "dry-run only, templates are not complied"
-        print "----------------"
-        traceback.print_exc(file=sys.stdout)
-        print "----------------"
-        print "Please run pywps to compile templates"
-else:
-    print "dry-run only, templates are not complied"
