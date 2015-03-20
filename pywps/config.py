c7d32eb798127741e11fc33894f098a3c907c60e
Calin Ciociu <calin.ciociu@gmail.com>
2015-03-20 16:15:40 +0100

    Set default values for the configuration tempPath is set according to OS temp folder

--- pywps/config.py
+++ pywps/config.py
@@ -1,32 +1,34 @@
 import os
 import ConfigParser
+import tempfile
 
 config = None
 
 
-def get_config_value(*args):
+def get_config_value(section, option):
     """Get desired value from  configuration files
 
     :param section: section in configuration files
     :type section: string
-    :param key: key in the section
-    :type key: string
+    :param option: option in the section
+    :type option: string
     :returns: value found in the configuration file
     """
 
-    value = None
+    value = ''
 
     if not config:
         load_configuration()
 
-    if config.has_option(*args):
-        value = config.get(*args)
+    if config.has_section(section):
+        if config.has_option(section, option):
+            value = config.get(section, option)
 
-    # Convert Boolean string to real Boolean values
-    if value.lower() == "false":
-        value = False
-    elif value.lower() == "true":
-        value = True
+            # Convert Boolean string to real Boolean values
+            if value.lower() == "false":
+                value = False
+            elif value.lower() == "true":
+                value = True
 
     return value
 
@@ -38,17 +40,53 @@ def load_configuration():
     """
     global config
 
-    default_cfg = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)),
-        os.path.pardir,
-        "default.cfg")
+    config = ConfigParser.SafeConfigParser()
 
+    # Set default values
+    config.add_section('wps')
+    config.set('wps', 'encoding', 'utf-8')
+    config.set('wps', 'title', 'PyWPS Server')
+    config.set('wps', 'version', '1.0.0')
+    config.set('wps', 'abstract', '')
+    config.set('wps', 'fees', 'NONE')
+    config.set('wps', 'constraint', 'NONE')
+    config.set('wps', 'serveraddress', 'http://')
+    config.set('wps', 'keywords', '')
+    config.set('wps', 'lang', 'en-CA')
+
+    config.add_section('provider')
+    config.set('provider', 'providerName', 'Your Company Name')
+    config.set('provider', 'individualName', 'Your Name')
+    config.set('provider', 'positionName', 'Your Position')
+    config.set('provider', 'role', 'Your Role')
+    config.set('provider', 'deliveryPoint', 'Street')
+    config.set('provider', 'city', 'City')
+    config.set('provider', 'postalCode', '000 00')
+    config.set('provider', 'country', 'LU')
+    config.set('provider', 'electronicalMailAddress', 'login@server.org')
+    config.set('provider', 'providerSite', 'http://foo.bar')
+    config.set('provider', 'phoneVoice', 'False')
+    config.set('provider', 'phoneFacsimile', 'False')
+    config.set('provider', 'administrativeArea', 'False')
+    config.set('provider', 'onlineResource', 'http://foo.bar')
+    config.set('provider', 'hoursOfService', '00:00-24:00')
+    config.set('provider', 'contactInstructions', 'NONE')
+
+    config.add_section('server')
+    config.set('server', 'maxoperations', '30')
+    config.set('server', 'maxinputparamlength', '1024')
+    config.set('server', 'maxfilesize', '3mb')
+    config.set('server', 'tempPath', tempfile.gettempdir())
+    config.set('server', 'processesPath', '')
+    config.set('server', 'outputUrl', '')
+    config.set('server', 'outputPath', '')
+    config.set('server', 'logFile', '')
+    config.set('server', 'logLevel', 'INFO')
+
+    # Read configuration file
     pywps_cfg = os.path.join(
         os.path.dirname(os.path.abspath(__file__)),
         os.path.pardir,
         "pywps.cfg")
-
-    files = [default_cfg, pywps_cfg]
-
-    config = ConfigParser.SafeConfigParser()
+    files = [pywps_cfg]
     config.read(files)
