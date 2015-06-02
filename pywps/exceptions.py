3f03d1e2590ec52e37549fbe28c0aeccdddec904
Luís de Sousa <luis.a.de.sousa@gmail.com>
2015-06-02 16:55:15 +0200

    Comments on _compat, exceptions, extent and wpsserver.

--- pywps/exceptions.py
+++ pywps/exceptions.py
@@ -5,6 +5,30 @@ Based on OGC OWS, WPS and
 
 http://lists.opengeospatial.org/pipermail/wps-dev/2013-October/000335.html
 """
+# Author:    Alex Morega & Calin Ciociu
+#            
+# License:
+#
+# Web Processing Service implementation
+# Copyright (C) 2015 PyWPS Development Team, represented by Jachym Cepicky
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to
+# deal in the Software without restriction, including without limitation the
+# rights to use, copy, modify, merge, publish, distribute, sublicense, and/or
+# sell copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+# 
+# The above copyright notice and this permission notice shall be included in
+# all copies or substantial portions of the Software.
+# 
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS
+# IN THE SOFTWARE.
 
 from werkzeug.exceptions import HTTPException, BadRequest, MethodNotAllowed
 from werkzeug._compat import text_type
