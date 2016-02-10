ba3e09ad587a24b1d56b5f966bd3b6a5269f966b
Tom Kralidis <tomkralidis@gmail.com>
2016-02-10 10:14:06 -0500

    mock pip dependencies for docs building on RTD

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -12,6 +12,10 @@
 # serve to show the default.
 
 import sys, os
+import mock
+
+sys.modules['GDAL'] = mock.Mock()
+sys.modules['lxml'] = mock.Mock()
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
