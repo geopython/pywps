d8bdb75787f708490199651442244a2269d105f8
System Administrator <jan.rudolf.steeldog@gmail.com>
2016-06-16 22:48:33 +0200

    Cosmetic updates in process control.

--- pywps/server/app/main.py
+++ pywps/server/app/main.py
@@ -15,6 +15,7 @@ class ServerConnection():
 		self.application = Flask(__name__)
 
 		self.db_cnt = postgresql.connect("dbname= 'pywps' user='janrudolf' password='1Straskov-Vodochody12'")
+		self.db_cursor = self.db_cnt.cursor()
 
 		self.wps = Service(processes=processes)
 
@@ -31,80 +32,47 @@ class ServerConnection():
 		def wps_process_stop(process_id):
 			sql_query = "SELECT pid FROM pywps_requests WHERE uuid = '{}';".format(process_id)
 
-			cur = self.db_cnt.cursor()
-			cur.execute(sql_query)
+			self.db_cursor.execute(sql_query)
 
-			data = cur.fetchone()
+			data = self.db_cursor.fetchone()
 
 			if data:
 				pid = data[0]
 
 				try:
-					process_p = psutil.Process(pid=pid)
-
+					process = psutil.Process(pid=pid)
 				except psutil.NoSuchProcess:
-					#no process with this pid
 					print("Error: No such process with {}".format(pid))
 				except psutil.ZombieProcess:
 					print("Error: Zombie process")
 				except psutil.AccessDenied:
 					print("Error: Access denied")
 
-				process_p.terminate()
-
-				print('Process {} killed...'.format(pid))
+				process.terminate()
 
 			return 'Proces killed - %s' % process_id
 
 		@self.application.route('/processes/pause/<process_id>')
 		def wps_process_pause(process_id):
-			"""
-			print("ahoj")
-			print(process_id)
-			sql = "SELECT pid FROM pywps_requests;"
-			print(sql)
-			
-			database = configuration.get_config_value('server', 'logdatabase')
-			
-			cnt = sqlite3.connect(database)
-
-			cur = cnt.cursor()
-			res = cur.execute(sql)
-
-			data = res.fetchall()
-
-			print(data)
-
-			if data:
-				pid = data[0]
-			
-			dblog.close_connection()
-			"""
-
 			sql_query = "SELECT pid FROM pywps_requests WHERE uuid = '{}';".format(process_id)
 
-			cur = self.db_cnt.cursor()
-			cur.execute(sql_query)
+			self.db_cursor.execute(sql_query)
 
-			data = cur.fetchone()
+			data = self.db_cursor.fetchone()
 
 			if data:
 				pid = data[0]
 
 				try:
-					process_p = psutil.Process(pid=pid)
-
+					process = psutil.Process(pid=pid)
 				except psutil.NoSuchProcess:
-					#no process with this pid
 					print("Error: No such process with {}".format(pid))
 				except psutil.ZombieProcess:
 					print("Error: Zombie process")
 				except psutil.AccessDenied:
 					print("Error: Access denied")
 
-				process_p.suspend()
-
-				print('Process pid={} suspended...'.format(pid))
+				process.suspend()
 
 			return 'Process suspend - %s' % process_id
 
@@ -112,28 +80,23 @@ class ServerConnection():
 		def wps_process_resume(process_id):
 			sql_query = "SELECT pid FROM pywps_requests WHERE uuid = '{}';".format(process_id)
 
-			cur = self.db_cnt.cursor()
-			cur.execute(sql_query)
+			self.db_cursor.execute(sql_query)
 
-			data = cur.fetchone()
+			data = self.db_cursor.fetchone()
 
 			if data:
 				pid = data[0]
 
 				try:
-					process_p = psutil.Process(pid=pid)
-
+					process = psutil.Process(pid=pid)
 				except psutil.NoSuchProcess:
-					#no process with this pid
 					print("Error: No such process with {}".format(pid))
 				except psutil.ZombieProcess:
 					print("Error: Zombie process")
 				except psutil.AccessDenied:
 					print("Error: Access denied")
 
-				process_p.resume()
-
-				print('Process pid={} resuming...'.format(pid))
+				process.resume()
 
 			return 'Process resume - %s' % process_id
 
