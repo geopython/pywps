383eff33beab375fe0c4f7a040d2d55ffd297fe0
System Administrator <jan.rudolf.steeldog@gmail.com>
2016-06-17 11:09:59 +0200

    Create get_process_by_uuid method and make clear code in URL handlers

--- pywps/server/app/main.py
+++ pywps/server/app/main.py
@@ -14,23 +14,34 @@ class ServerConnection():
 
 		self.application = Flask(__name__)
 
-		self.db_cnt = postgresql.connect("dbname= 'pywps' user='janrudolf' password=''")
-		self.db_cursor = self.db_cnt.cursor()
+		self.db_connect = postgresql.connect("dbname= 'pywps' user='janrudolf' password=''")
+		self.db_cursor = self.db_connect.cursor()
 
 		self.wps = Service(processes=processes)
 
 	def __del__(self):
-		self.db_cnt.close()
+		self.db_connect.close()
 
-	def get_process_pid(process_id):
-		sql_query = "SELECT pid FROM pywps_requests WHERE uuid = '{}';".format(process_id)
+	def get_process_by_uuid(self, uuid):
+		sql_query = "SELECT pid FROM pywps_requests WHERE uuid = '{}';".format(uuid)
 
 		self.db_cursor.execute(sql_query)
 		
 		data = self.db_cursor.fetchone()
 
 		if data:
-			return data[0]
+			pid = data[0]
+
+			try:
+				process = psutil.Process(pid=pid)
+			except psutil.NoSuchProcess:
+				print("Error: No such process with {}".format(pid))
+			except psutil.ZombieProcess:
+				print("Error: Zombie process")
+			except psutil.AccessDenied:
+				print("Error: Access denied")
+
+			return process
 		
 		return None
 
@@ -43,59 +54,38 @@ class ServerConnection():
 		def wps():
 			return self.wps
 
-		@self.application.route('/processes/stop/<process_id>')
-		def wps_process_stop(process_id):
-			pid = self.get_process_pid(process_id)
+		@self.application.route('/processes/stop/<uuid>')
+		def wps_process_stop(uuid):
+			process = self.get_process_by_uuid(uuid)
 
-			if pid:
-				try:
-					process = psutil.Process(pid=pid)
-				except psutil.NoSuchProcess:
-					print("Error: No such process with {}".format(pid))
-				except psutil.ZombieProcess:
-					print("Error: Zombie process")
-				except psutil.AccessDenied:
-					print("Error: Access denied")
-
+			if process:
 				process.terminate()
+			else:
+				return 'No known process with uuid=%s' % uuid
 
-			return 'Proces killed - %s' % process_id
+			return 'Proces killed - %s' % uuid
 
-		@self.application.route('/processes/pause/<process_id>')
-		def wps_process_pause(process_id):
-			pid = self.get_process_pid(process_id)
+		@self.application.route('/processes/pause/<uuid>')
+		def wps_process_pause(uuid):
+			process = self.get_process_by_uuid(uuid)
 
-			if pid:
-				try:
-					process = psutil.Process(pid=pid)
-				except psutil.NoSuchProcess:
-					print("Error: No such process with {}".format(pid))
-				except psutil.ZombieProcess:
-					print("Error: Zombie process")
-				except psutil.AccessDenied:
-					print("Error: Access denied")
-
+			if process:
 				process.suspend()
+			else:
+				return 'No known process with uuid=%s' % uuid
 
-			return 'Process suspend - %s' % process_id
+			return 'Process suspend - %s' % uuid
 
-		@self.application.route('/processes/resume/<process_id>')
-		def wps_process_resume(process_id):
-			pid = self.get_process_pid(process_id)
+		@self.application.route('/processes/resume/<uuid>')
+		def wps_process_resume(uuid):
+			process = self.get_process_by_uuid(uuid)
 
-			if pid:
-				try:
-					process = psutil.Process(pid=pid)
-				except psutil.NoSuchProcess:
-					print("Error: No such process with {}".format(pid))
-				except psutil.ZombieProcess:
-					print("Error: Zombie process")
-				except psutil.AccessDenied:
-					print("Error: Access denied")
-
+			if process:
 				process.resume()
+			else:
+				return 'No known process with uuid=%s' % uuid
 
-			return 'Process resume - %s' % process_id
+			return 'Process resume - %s' % uuid
 
 		@self.application.route('/manage') # or /processes
 		def wps_manage():
