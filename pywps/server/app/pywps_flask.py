6e9620ed2aade63d8742cb62f406c1d9fc6cb3bf
System Administrator <jan.rudolf.steeldog@gmail.com>
2016-07-20 10:14:15 +0200

    Change of the configuration loading

--- pywps/server/app/pywps_flask.py
+++ pywps/server/app/pywps_flask.py
@@ -11,13 +11,11 @@ class PyWPSFlask(Flask):
 		super(PyWPSFlask, self).__init__(import_name)
 
 	def load_pywps_app(self, processes, configuration_file):
-		#load configuration file
-		configuration.load_configuration(configuration_file)
 		#store processes in method attribute
 		self.pywps_processes = processes
 		#WPS service
 		from pywps import Service
-		self.pywps_wps_service = Service(processes=processes)
+		self.pywps_wps_service = Service(processes=processes, cfgfiles=[configuration_file])
 		#set database connection
 		self.config['SQLALCHEMY_DATABASE_URI'] = configuration.get_config_value('server', 'SQLAlchemyDatabaseUri')
 		
