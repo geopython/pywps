3b22a302f92478fd0979c9baa8f39bdea3db68e2
System Administrator <jan.rudolf.steeldog@gmail.com>
2016-07-24 16:51:26 +0200

    Be able to run PyWPS locally with the Flask's development server

--- pywps/server/app/pywps_flask.py
+++ pywps/server/app/pywps_flask.py
@@ -6,17 +6,18 @@ from pywps import configuration
 class PyWPSFlask(Flask):
 	def __init__(self, import_name):
 		self.pywps_processes = None
-		self.pywps_wps_service = None
+		self.pywps_service = None
 
 		super(PyWPSFlask, self).__init__(import_name)
 
-	def load_pywps_app(self, processes, configuration_file):
+	def run_pywps(self, processes=None, configuration_file=None, run_locally=None):
 		#store processes in method attribute
 		self.pywps_processes = processes
 		#WPS service
 		from pywps import Service
-		self.pywps_wps_service = Service(processes=processes, cfgfiles=[configuration_file])
+		self.pywps_service = Service(processes=processes, cfgfiles=[configuration_file])
 		#set database connection
 		self.config['SQLALCHEMY_DATABASE_URI'] = configuration.get_config_value('server', 'SQLAlchemyDatabaseUri')
 		
-		return True
+		if run_locally:
+			self.run(host='0.0.0.0')
