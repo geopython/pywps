47fdc02590c2b7fbd3e082391bce2b2a9da7b593
Jan Rudolf <jan.rudolf.steeldog@gmail.com>
2016-07-25 16:49:54 +0200

    change of identation style to spaces - IDE complaint

--- pywps/server/app/pywps_flask.py
+++ pywps/server/app/pywps_flask.py
@@ -4,20 +4,23 @@ from pywps import configuration
 
 
 class PyWPSFlask(Flask):
-	def __init__(self, import_name):
-		self.pywps_processes = None
-		self.pywps_service = None
+    def __init__(self, import_name):
+        self.pywps_processes = None
+        self.pywps_service = None
 
-		super(PyWPSFlask, self).__init__(import_name)
+        super(PyWPSFlask, self).__init__(import_name)
 
-	def run_pywps(self, processes=None, configuration_file=None, run_locally=None):
-		#store processes in method attribute
-		self.pywps_processes = processes
-		#WPS service
-		from pywps import Service
-		self.pywps_service = Service(processes=processes, cfgfiles=[configuration_file])
-		#set database connection
-		self.config['SQLALCHEMY_DATABASE_URI'] = configuration.get_config_value('server', 'SQLAlchemyDatabaseUri')
-		
-		if run_locally:
-			self.run(host='0.0.0.0')
+    def run_pywps(self, processes=None, configuration_file=None, run_locally=None):
+        #store processes in method attribute
+        self.pywps_processes = processes
+
+        #WPS service
+        from pywps import Service
+
+        self.pywps_service = Service(processes=processes, cfgfiles=[configuration_file])
+        #set database connection
+        self.config['SQLALCHEMY_DATABASE_URI'] = configuration.get_config_value('server', 'SQLAlchemyDatabaseUri')
+        self.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+        if run_locally:
+            self.run(host='0.0.0.0')
