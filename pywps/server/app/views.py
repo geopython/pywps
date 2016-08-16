bd0e3428a9afe7c59a17bc2c293441e7c085a5c5
Jan Rudolf <jan.rudolf.steeldog@gmail.com>
2016-08-16 15:03:51 +0200

    Fix identation, empty results

--- pywps/server/app/views.py
+++ pywps/server/app/views.py
@@ -143,49 +143,49 @@ def pywps_manage_page():
 
 @application.route('/manage/table-entries', methods=['POST'])
 def pywps_processes_table_entries():
-	error = False
+    error = False
 
-	data = flask.request.get_json()
+    data = flask.request.get_json()
 
-	query = models.Request.query
+    query = models.Request.query
 
-	data_status = str(data['status']) if str(data['status']) != 'none' else False
-	data_operation = str(data['operation']) if str(data['operation']) != '0' else False
-	data_identifier = str(data['identifier']) if str(data['identifier']) != '0' else False
-	try:
-		data_pid = int(data['pid'])
-	except:
-		data_pid = 0 if (len(str(data['pid'])) > 0) else None
+    data_status = str(data['status']) if str(data['status']) != 'none' else False
+    data_operation = str(data['operation']) if str(data['operation']) != '0' else False
+    data_identifier = str(data['identifier']) if str(data['identifier']) != '0' else False
+    try:
+        data_pid = int(data['pid'])
+    except:
+        data_pid = 0 if (len(str(data['pid'])) > 0) else None
 
-	data_uuid = str(data['uuid'])
+    data_uuid = str(data['uuid'])
 
-	if not error and data_status:
+    if not error and data_status:
 
-		if data_status == "running":
-			query = query.filter(
-				sqlalchemy.or_(
-					models.Request.status == str(wps_response_status.STORE_AND_UPDATE_STATUS), models.Request.status == str(wps_response_status.STORE_STATUS)
-				)
-			)
-		elif data_status == "paused":
-			query = query.filter(models.Request.status == str(wps_response_status.PAUSED_STATUS))
-		elif data_status == "stopped":
-			query = query.filter(models.Request.status == str(wps_response_status.STOPPED_STATUS))
-		elif data_status == "finished":
-			query = query.filter(models.Request.status == str(wps_response_status.DONE_STATUS))
+        if data_status == "running":
+            query = query.filter(
+                sqlalchemy.or_(
+                    models.Request.status == str(wps_response_status.STORE_AND_UPDATE_STATUS), models.Request.status == str(wps_response_status.STORE_STATUS)
+                )
+            )
+        elif data_status == "paused":
+            query = query.filter(models.Request.status == str(wps_response_status.PAUSED_STATUS))
+        elif data_status == "stopped":
+            query = query.filter(models.Request.status == str(wps_response_status.STOPPED_STATUS))
+        elif data_status == "finished":
+            query = query.filter(models.Request.status == str(wps_response_status.DONE_STATUS))
 
-	if not error and  data_operation:
-		query = query.filter(models.Request.operation == data_operation)
+    if not error and  data_operation:
+        query = query.filter(models.Request.operation == data_operation)
 
-	if not error and  data_identifier:
-		query = query.filter(models.Request.identifier == data_identifier)
+    if not error and  data_identifier:
+        query = query.filter(models.Request.identifier == data_identifier)
 
-	if not error and data_pid != None:
-		query = query.filter(models.Request.pid == data_pid)
+    if not error and data_pid != None:
+        query = query.filter(models.Request.pid == data_pid)
 
-	if not error and len(data_uuid) > 0:
-		query = query.filter(models.Request.uuid.like('%{}%'.format(data_uuid)))
+    if not error and len(data_uuid) > 0:
+        query = query.filter(models.Request.uuid.like('%{}%'.format(data_uuid)))
 
-	query = query.order_by(models.Request.time_start)
+    query = query.order_by(models.Request.time_start)
 
-	return flask.render_template('manage_processes_table_entries.html', processes=query, wps_response_status=wps_response_status)
+    return flask.render_template('manage_processes_table_entries.html', processes=query.all(), wps_response_status=wps_response_status)
