81b74a15c6e06c603807565df160a5b0653ca2ab
Jan Rudolf <jan.rudolf.steeldog@gmail.com>
2016-08-18 12:05:50 +0200

    change alerts to console.log

--- pywps/server/app/static/js/scripts.js
+++ pywps/server/app/static/js/scripts.js
@@ -1,140 +1,143 @@
 function pywps_pause_process(uuid) {
-	
-	var xhr = $.ajax( {
-		url: "/processes/" + uuid,
-		method: "PUT",
-		contentType: "application/json;charset=UTF-8",
-		data: JSON.stringify({"action": "pause"})
-	} );
 
-	xhr.done( function (data) {
+    var xhr = $.ajax( {
+        url: "/processes/" + uuid,
+        method: "PUT",
+        contentType: "application/json;charset=UTF-8",
+        data: JSON.stringify({"action": "pause"})
 
-		if (!data.error) {
-			$("#pause-btn-" + uuid).removeClass("display-block");
-			$("#resume-btn-" + uuid).addClass("display-block");
+    } );
 
-			$("#status-text-" + uuid).html("Paused");
+    xhr.done( function (data) {
 
-		} else{
-			alert("Error - " + data.error);
+        if (!data.error) {
+            $("#pause-btn-" + uuid).removeClass("display-block");
+            $("#resume-btn-" + uuid).addClass("display-block");
 
-		}
+            $("#status-text-" + uuid).html("Paused");
 
-	} );
+        } else{
+            alert("Error - " + data.error);
 
-	xhr.fail( function () {
-		alert("error");
+        }
 
-	} );
+    } );
+
+    xhr.fail( function () {
+        console.log("Error - pywps_pause_process");
+
+    } );
 
 }
 
 
 function pywps_stop_process(uuid) {
-	var xhr = $.ajax( {
-	  url: "/processes/" + uuid,
-	  method: "DELETE"
+    var xhr = $.ajax( {
+      url: "/processes/" + uuid,
+      method: "DELETE"
 
-	} ); 
+    } );
 
-	xhr.done( function( data ) {
-	 	console.log("DONE stop " + uuid);
+    xhr.done( function( data ) {
+        console.log("DONE stop " + uuid);
 
-		if (!data.error) {
-			$("#pause-btn-" + uuid).removeClass("display-block");
-			$("#resume-btn-" + uuid).removeClass("display-block");
-			$("#stop-btn-" + uuid).removeClass("display-block");
+        if (!data.error) {
+            $("#pause-btn-" + uuid).removeClass("display-block");
+            $("#resume-btn-" + uuid).removeClass("display-block");
+            $("#stop-btn-" + uuid).removeClass("display-block");
 
-			$("#action-btn-" + uuid).html("-");
+            $("#action-btn-" + uuid).html("-");
 
-			$("#status-text-" + uuid).html("Stopped");
+            $("#status-text-" + uuid).html("Stopped");
 
-		} else{
-			alert("Error - " + data.error);
+        } else{
+            alert("Error - " + data.error);
 
-		}
+        }
 
-	} ); 
+    } );
 
-	xhr.fail( function () {
-		alert("error");
+    xhr.fail( function () {
+        console.log("Error - pywps_stop_process");
 
-	} );
+    } );
 
 }
 
 function pywps_resume_process(uuid) {
-	var xhr = $.ajax( {
-	  url: "/processes/" + uuid,
-	  method: "PUT",
-	  contentType: "application/json;charset=UTF-8",
-	  data: JSON.stringify({"action": "resume"})
-	} );
+    var xhr = $.ajax( {
+      url: "/processes/" + uuid,
+      method: "PUT",
+      contentType: "application/json;charset=UTF-8",
+      data: JSON.stringify({"action": "resume"})
+    } );
 
-	xhr.done(function (data) {
-		console.log("DONE resume " + uuid);
+    xhr.done(function (data) {
+        console.log("DONE resume " + uuid);
 
-		if (!data.error) {
-			$("#pause-btn-" + uuid).addClass("display-block");
-			$("#resume-btn-" + uuid).removeClass("display-block");
+        if (!data.error) {
+            $("#pause-btn-" + uuid).addClass("display-block");
+            $("#resume-btn-" + uuid).removeClass("display-block");
 
-			$("#status-text-" + uuid).html("Running");
+            $("#status-text-" + uuid).html("Running");
 
-		} else {
-			alert("Error - " + data.error);
+        } else {
+            alert("Error - " + data.error);
 
-		}
+        }
 
-	} );
+    } );
 
-	xhr.fail( function() {
-		alert("error");
-	} );
+    xhr.fail( function() {
+        alert("Error - pywps_resume_process");
+
+    } );
 
 }
 
 function get_filter() {
-	var filter = {
-		status: $('#filter-status').val(),
-		operation: $('#filter-operation').val(),
-		identifier: $('#filter-identifier').val(),
-		uuid: $('#filter-uuid').val(),
-		pid: $('#filter-pid').val()
-	}
+    var filter = {
+        status: $('#filter-status').val(),
+        operation: $('#filter-operation').val(),
+        identifier: $('#filter-identifier').val(),
+        uuid: $('#filter-uuid').val(),
+        pid: $('#filter-pid').val()
+    }
 
-	return filter;
+    return filter;
 }
 
 function clear_filter() {
-	$('#filter-status').val(0);
-	$('#filter-operation').val(0);
-	$('#filter-identifier').val(0);
-	$('#filter-uuid').val("");
-	$('#filter-pid').val("");
+    $('#filter-status').val(0);
+    $('#filter-operation').val(0);
+    $('#filter-identifier').val(0);
+    $('#filter-uuid').val("");
+    $('#filter-pid').val("");
 }
 
 function pywps_refresh_processes_table () {
-	var filter = get_filter();
+    var filter = get_filter();
 
-	var xhr = $.ajax( {
-	  url: "/manage/table-entries",
-	  method: "POST",
-	  contentType: 'application/json;charset=UTF-8',
-	  data: JSON.stringify(filter)
-	} );
+    var xhr = $.ajax( {
+      url: "/manage/table-entries",
+      method: "POST",
+      contentType: 'application/json;charset=UTF-8',
+      data: JSON.stringify(filter)
+    } );
 
-	xhr.done(function (data) {
-		$('#processes_table').html(data);
+    xhr.done(function (data) {
+        $('#processes_table').html(data);
 
-	} );
+    } );
 
-	xhr.fail( function() {
-	} );
+    xhr.fail( function() {
+        console.log("Error - pywps_refresh_processes_table");
+
+    } );
 }
 
 //check the processes data every second (1000ms)
 window.setInterval(pywps_refresh_processes_table, 1000);
-
 
 
 
