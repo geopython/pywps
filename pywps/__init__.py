4ccba56987f9d30549052cf327c7f31ed1d453aa
Jachym Cepicky <jachym.cepicky@gmail.com>
2016-11-10 17:15:03 +0100

    Cleaning copyright holder in files

--- pywps/__init__.py
+++ pywps/__init__.py
@@ -67,25 +67,6 @@ example how to use this module::
     response for you.
 
 """
-# Author:   Jachym Cepicky
-#           http://les-ejk.cz
-# License:
-#
-# Web Processing Service implementation
-# Copyright (C) 2006 Jachym Cepicky
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 
 
 __all__ = [ "Parser","processes", "Process", "Exceptions", "Wps", "Templates","Template","XSLT","Ftp"]
