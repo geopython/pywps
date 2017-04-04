2ebaa158c04522e8e0d7e6d262fa94b270f31e0f
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-04 19:33:22 +0200

    working on pywps launcher

--- pywps/app/processing.py
+++ pywps/app/processing.py
@@ -40,10 +40,10 @@ SLURM_TMPL = """\
 #SBATCH -e emu.err
 #SBATCH -o emu.out
 #SBATCH -J emu
-#SBATCH --time=00:30:00
-set -eo pipefail -o nounset
-source activate emu
-python -c 'from pywps.app.processing import launch_slurm_job\nlaunch_slurm_job()'
+#SBATCH --time=12:30:00
+#set -eo pipefail -o nounset
+export PATH="/home/testuser/anaconda/bin:$PATH"
+source activate emu;python -c 'from pywps.app.processing import launch_slurm_job\nlaunch_slurm_job()'
 """
 
 
@@ -56,12 +56,25 @@ class Slurm(Processing):
         import dill
         from pathos import SSH_Launcher
         from pathos.secure import Copier
+        # marshall process
         dill.dump(self, open('/tmp/marshalled', 'w'))
+        # copy marshalled file to remote
         copier = Copier("marshalled")
-        copier.config(source='/tmp/marshalled', destination='testuser@localhost:/tmp/marshalled')
+        copier.config(source='/tmp/marshalled', destination='testuser@docker.example.com:/tmp/marshalled')
+        copier.launch()
+        # write sbatch script
+        with open("/tmp/emu.submit", 'w') as fp:
+            fp.write(SLURM_TMPL)
+        # copy marshalled file to remote
+        copier = Copier("sbatch")
+        copier.config(source='/tmp/emu.submit', destination='testuser@docker.example.com:/tmp/emu.submit')
+        copier.launch()
+        # run remote pywps process
         launcher = SSH_Launcher("test")
-        launcher.config(command="sbatch", rhost="localhost", background=True)
+        launcher.config(command="sbatch /tmp/emu.submit", host="testuser@docker.example.com", background=False)
+        #launcher.config(command="hostname", host="testuser@docker.example.com", background=False)
         launcher.launch()
+        print launcher.response()
 
 
 def launch_slurm_job(filename=None):
