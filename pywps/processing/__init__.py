bdab34c29b60afbe8feb506e1f01b1e361dadaa7
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-04-05 19:26:59 +0200

    showing slurm response

--- pywps/processing/__init__.py
+++ pywps/processing/__init__.py
@@ -8,12 +8,16 @@ import pywps.configuration as config
 from pywps.processing.basic import MultiProcessing
 from pywps.processing.slurm import Slurm
 
+import logging
+LOGGER = logging.getLogger("PYWPS")
+
 MULTIPROCESSING = 'multiprocessing'
 SLURM = 'slurm'
 
 
 def Process(process, wps_request, wps_response):
     mode = config.get_config_value("extra", "mode")
+    LOGGER.info("Configured processing mode: %s", mode)
     if mode == SLURM:
         return Slurm(process, wps_request, wps_response)
     else:
