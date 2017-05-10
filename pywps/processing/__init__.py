709d594111944f29af446e355c28a131c664ba6c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2017-05-10 10:56:33 +0200

    update comment

--- pywps/processing/__init__.py
+++ pywps/processing/__init__.py
@@ -17,6 +17,12 @@ DEFAULT = MULTIPROCESSING
 
 
 def Process(process, wps_request, wps_response):
+    """
+    Factory method (looking like a class) to return the
+    configured processing class.
+
+    :return: instance of :class:`pywps.processing.Processing`
+    """
     mode = config.get_config_value("processing", "mode")
     LOGGER.info("Processing mode: %s", mode)
     if mode == SLURM:
