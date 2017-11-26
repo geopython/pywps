1c0cc4df4137f186eb75386f1a4e0367e91bce0a
lazaa32 <ad.laza32@gmail.com>
2017-11-26 22:24:03 +0100

    Input as reference into docker done

--- pywps/app/Process.py
+++ pywps/app/Process.py
@@ -138,11 +138,6 @@ class Process(object):
     def execute(self, wps_request, uuid):
         self._set_uuid(uuid)
 
-        # TODO just for testing purpose - changed to True
-        # nebo nechat False ale upravit testovaci proces
-        # store_supported=True,
-        # status_supported=True
-
         self.async = False
         response_cls = get_response("execute")
         wps_response = response_cls(wps_request, process=self, uuid=self.uuid)
@@ -204,7 +199,6 @@ class Process(object):
             # run immedietly
             if running < maxparallel or maxparallel == -1:
                 self._run_async(wps_request, wps_response)
-
             # try to store for later usage
             else:
                 wps_response = self._store_process(stored,
