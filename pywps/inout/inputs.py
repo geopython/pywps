dc8dfd51ae11d6d3caffebbc70315010f51e3ade
David Caron <dcaron05@gmail.com>
2019-03-08 11:51:50 -0500

    move json serialization methods from WPSRequest.json to inout.inputs...
    I put them in inout.inputs instead of inout.basic because
    ComplexInput.json was already there and json methods for outputs are in
    inout.outputs, not in inout.basic

--- pywps/inout/inputs.py
+++ pywps/inout/inputs.py
@@ -6,10 +6,12 @@
 import lxml.etree as etree
 import six
 from pywps import configuration
+from pywps.exceptions import InvalidParameterValue
+from pywps.inout.formats import Format
 from pywps.inout import basic
 from copy import deepcopy
 from pywps.validator.mode import MODE
-from pywps.inout.literaltypes import AnyValue
+from pywps.inout.literaltypes import AnyValue, NoValue, ValuesReference, AllowedValue
 
 
 class BoundingBoxInput(basic.BBoxInput):
@@ -29,7 +31,7 @@ class BoundingBoxInput(basic.BBoxInput):
                      should be :class:`pywps.app.Common.Metadata` objects.
     """
 
-    def __init__(self, identifier, title, crss, abstract='', keywords=[],
+    def __init__(self, identifier, title, crss=None, abstract='', keywords=[],
                  dimensions=2, workdir=None, metadata=[], min_occurs=1,
                  max_occurs=1,
                  mode=MODE.NONE,
@@ -43,6 +45,58 @@ class BoundingBoxInput(basic.BBoxInput):
                                  default_type=default_type)
 
         self.as_reference = False
+
+    @property
+    def json(self):
+        """Get JSON representation of the input. It returns following keys in
+        the JSON object:
+
+            * identifier
+            * title
+            * abstract
+            * type
+            * crs
+            * bbox
+            * dimensions
+            * workdir
+            * mode
+        """
+        return {
+            'identifier': self.identifier,
+            'title': self.title,
+            'abstract': self.abstract,
+            'keywords': self.keywords,
+            'type': 'bbox',
+            'crs': self.crs,
+            'crss': self.crss,
+            'metadata': self.metadata,
+            'bbox': (self.ll, self.ur),
+            'dimensions': self.dimensions,
+            'workdir': self.workdir,
+            'mode': self.valid_mode,
+            'min_occurs': self.min_occurs,
+            'max_occurs': self.max_occurs
+        }
+
+    @classmethod
+    def from_json(cls, json_input):
+        instance = cls(
+            identifier=json_input['identifier'],
+            title=json_input['title'],
+            abstract=json_input['abstract'],
+            crss=json_input['crss'],
+            keywords=json_input['keywords'],
+            metadata=json_input['metadata'],
+            dimensions=json_input['dimensions'],
+            workdir=json_input['workdir'],
+            mode=json_input['mode'],
+            min_occurs=json_input['min_occurs'],
+            max_occurs=json_input['max_occurs'],
+        )
+        instance.ll = json_input['bbox'][0]
+        instance.ur = json_input['bbox'][1]
+
+        return instance
 
     def clone(self):
         """Create copy of yourself
@@ -121,18 +175,44 @@ class ComplexInput(basic.ComplexInput):
 
         return data
 
-    def _json_reference(self, data):
-        """Return Reference node
-        """
-        data["type"] = "reference"
-        data["href"] = self.url
-        return data
+    @classmethod
+    def from_json(cls, json_input):
+        instance = cls(
+            identifier=json_input['identifier'],
+            title=json_input.get('title'),
+            abstract=json_input.get('abstract'),
+            keywords=json_input.get('keywords', []),
+            workdir=json_input.get('workdir'),
+            metadata=json_input.get('metadata', []),
+            data_format=Format(
+                schema=json_input['data_format'].get('schema'),
+                extension=json_input['data_format'].get('extension'),
+                mime_type=json_input['data_format']['mime_type'],
+                encoding=json_input['data_format'].get('encoding')
+            ),
+            supported_formats=[
+                Format(
+                    schema=infrmt.get('schema'),
+                    extension=infrmt.get('extension'),
+                    mime_type=infrmt['mime_type'],
+                    encoding=infrmt.get('encoding')
+                ) for infrmt in json_input['supported_formats']
+            ],
+            mode=json_input.get('mode', MODE.NONE)
+        )
+        instance.as_reference = json_input.get('asreference', False)
+        if json_input.get('file'):
+            instance.file = json_input['file']
+        elif json_input.get('href'):
+            instance.url = json_input['href']
+        elif json_input.get('data'):
+            instance.data = json_input['data']
+
+        return instance
 
     def _json_data(self, data):
         """Return Data node
         """
-
-        data["type"] = "complex"
 
         if self.data:
 
@@ -189,7 +269,7 @@ class LiteralInput(basic.LiteralInput):
                      should be :class:`pywps.app.Common.Metadata` objects.
     """
 
-    def __init__(self, identifier, title, data_type='integer', workdir=None, abstract='', keywords=[],
+    def __init__(self, identifier, title=None, data_type='integer', workdir=None, abstract='', keywords=[],
                  metadata=[], uoms=None,
                  min_occurs=1, max_occurs=1,
                  mode=MODE.SIMPLE, allowed_values=AnyValue,
@@ -208,7 +288,77 @@ class LiteralInput(basic.LiteralInput):
 
         self.as_reference = False
 
+    @property
+    def json(self):
+        """Get JSON representation of the input
+        """
+        data = {
+            'identifier': self.identifier,
+            'title': self.title,
+            'abstract': self.abstract,
+            'keywords': self.keywords,
+            'type': 'literal',
+            'data_type': self.data_type,
+            'workdir': self.workdir,
+            'allowed_values': [value.json for value in self.allowed_values],
+            'mode': self.valid_mode,
+            'min_occurs': self.min_occurs,
+            'max_occurs': self.max_occurs,
+
+            # other values not set in the constructor
+            'data': self.data,
+        }
+        if self.uoms:
+            data["uoms"] = [uom.json for uom in self.uoms]
+        return data
+
+    @classmethod
+    def from_json(cls, json_input):
+
+        allowed_values = []
+        for allowed_value in json_input['allowed_values']:
+            if allowed_value['type'] == 'anyvalue':
+                allowed_values.append(AnyValue())
+            elif allowed_value['type'] == 'novalue':
+                allowed_values.append(NoValue())
+            elif allowed_value['type'] == 'valuesreference':
+                allowed_values.append(ValuesReference())
+            elif allowed_value['type'] == 'allowedvalue':
+                allowed_values.append(AllowedValue(
+                    allowed_type=allowed_value['allowed_type'],
+                    value=allowed_value['value'],
+                    minval=allowed_value['minval'],
+                    maxval=allowed_value['maxval'],
+                    spacing=allowed_value['spacing'],
+                    range_closure=allowed_value['range_closure']
+                ))
+
+        json_input['allowed_values'] = allowed_values
+        json_input['uoms'] = [basic.UOM(uom.get('uom')) for uom in json_input.get('uoms', [])]
+
+        data = json_input.pop('data', None)
+        json_input.pop('type')
+
+        instance = cls(**json_input)
+
+        instance.data = data
+
+        return instance
+
     def clone(self):
         """Create copy of yourself
         """
         return deepcopy(self)
+
+
+def input_from_json(json_data):
+    if json_data['type'] == 'complex':
+        inpt = ComplexInput.from_json(json_data)
+    elif json_data['type'] == 'literal':
+        inpt = LiteralInput.from_json(json_data)
+    elif json_data['type'] == 'bbox':
+        inpt = BoundingBoxInput.from_json(json_data)
+    else:
+        raise InvalidParameterValue("Input type not recognized: {}".format(json_data['type']))
+
+    return inpt
