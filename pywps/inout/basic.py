fed85283723b9f52ae097639996f9dd2fca52d23
Eran Pinhas <eran-pinhas@users.noreply.github.com>
2019-08-06 14:36:57 +0300

    Fix - set store file encoding to utf8

--- pywps/inout/basic.py
+++ pywps/inout/basic.py
@@ -297,7 +297,7 @@ class FileHandler(IOHandler):
     def data(self):
         """Read file and return content."""
         if self._data is None:
-            with open(self.file, mode=self._openmode()) as fh:
+            with open(self.file, mode=self._openmode(), encoding='utf8') as fh:
                 self._data = fh.read()
         return self._data
 
@@ -378,7 +378,7 @@ class DataHandler(FileHandler):
         """
         if self._file is None:
             self._file = self._build_file_name()
-            with open(self._file, self._openmode(self.data)) as fh:
+            with open(self._file, self._openmode(self.data), encoding='utf8') as fh:
                 fh.write(self.data)
 
         return self._file
@@ -453,7 +453,7 @@ class UrlHandler(FileHandler):
             raise FileSizeExceeded(error_message)
 
         try:
-            with open(self._file, 'wb') as f:
+            with open(self._file, 'wb', encoding='utf8') as f:
                 data_size = 0
                 for chunk in reference_file.iter_content(chunk_size=1024):
                     data_size += len(chunk)
