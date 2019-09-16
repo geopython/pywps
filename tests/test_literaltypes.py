b40032ec742fe83100cb18f6901002538b43c2c3
corrado9999 <corrado9999@users.noreply.github.com>
2019-09-16 13:47:28 +0200

    Add test for URI literal type

--- tests/test_literaltypes.py
+++ tests/test_literaltypes.py
@@ -74,6 +74,16 @@ class ConvertorTest(unittest.TestCase):
             convert_datetime(datetime.datetime(2016, 9, 22, 6)),
             datetime.datetime))
 
+    def test_anyuri(self):
+        """Test URI convertor"""
+        self.assertEqual(convert_anyURI("http://username:password@hostname.dom:port/deep/path/;params?query#fragment"),
+                         ('http', 'username:password@hostname.dom:port', '/deep/path/', 'params', 'k=v', 'fragment')
+                        )
+        self.assertEqual(convert_anyURI("file:///very/very/very/deep/path"),
+                         ('file', '', '/very/very/very/deep/path', '', '', '')
+                        )
+        with self.assertRaises(InvalidParameterValue):
+            convert_anyURI("ftp:///deep/path/;params?query#fragment")
 
 def load_tests(loader=None, tests=None, pattern=None):
     if not loader:
