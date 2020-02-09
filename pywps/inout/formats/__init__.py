07f4bb5b340182c011307c4546db77b35b8dd911
David Huard <huard.david@ouranos.ca>
2020-02-09 10:35:11 -0500

    Add NcML to supported FORMATS (#515)
    * added ncml to supported formats
    * added ncml schema

--- pywps/inout/formats/__init__.py
+++ pywps/inout/formats/__init__.py
@@ -18,7 +18,7 @@ import mimetypes
 _FORMATS = namedtuple('FORMATS', 'GEOJSON, JSON, SHP, GML, GPX, METALINK, META4, KML, KMZ, GEOTIFF,'
                                  'WCS, WCS100, WCS110, WCS20, WFS, WFS100,'
                                  'WFS110, WFS20, WMS, WMS130, WMS110,'
-                                 'WMS100, TEXT, DODS, NETCDF, LAZ, LAS, ZIP,'
+                                 'WMS100, TEXT, DODS, NETCDF, NCML, LAZ, LAS, ZIP,'
                                  'XML')
 
 
@@ -188,6 +188,7 @@ FORMATS = _FORMATS(
     Format('text/plain', extension='.txt'),
     Format('application/x-ogc-dods', extension='.nc'),
     Format('application/x-netcdf', extension='.nc', encoding='base64'),
+    Format('application/ncML+xml', extension='.ncml', schema="ncml/2.2/ncml-2.2.xsd"),
     Format('application/octet-stream', extension='.laz'),
     Format('application/octet-stream', extension='.las'),
     Format('application/zip', extension='.zip', encoding='base64'),
