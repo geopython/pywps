4f3c42c8b54ad11eec1cc19190bf7bd131f0dd90
Pingu Carsti <ehbrecht@dkrz.de>
2020-02-13 13:46:10 +0100

    fix 2to3

--- tests/test_execute.py
+++ tests/test_execute.py
@@ -290,7 +290,7 @@ class ExecuteTest(unittest.TestCase):
         """
         my_process = create_complex_proces()
         service = Service(processes=[my_process])
-        self.assertEqual(len(list(service.processes.keys())), 1)
+        self.assertEqual(len(service.processes), 1)
         self.assertTrue(service.processes['my_complex_process'])
 
         class FakeRequest():
@@ -347,7 +347,7 @@ class ExecuteTest(unittest.TestCase):
         """
         my_process = create_complex_proces()
         service = Service(processes=[my_process])
-        self.assertEqual(len(list(service.processes.keys())), 1)
+        self.assertEqual(len(service.processes), 1)
         self.assertTrue(service.processes['my_complex_process'])
 
         class FakeRequest():
