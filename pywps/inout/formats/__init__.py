07a3480363d1ec598c4b9eaaafe318bda97bbab8
Trevor James Smith <10819524+Zeitsperre@users.noreply.github.com>
2021-03-29 10:59:34 -0400

    Update pywps/inout/formats/__init__.py

--- pywps/inout/formats/__init__.py
+++ pywps/inout/formats/__init__.py
@@ -19,7 +19,7 @@ _FORMATS = namedtuple('FORMATS', 'GEOJSON, JSON, SHP, GML, GPX, METALINK, META4,
                                  'WCS, WCS100, WCS110, WCS20, WFS, WFS100,'
                                  'WFS110, WFS20, WMS, WMS130, WMS110,'
                                  'WMS100, TEXT, DODS, NETCDF, NCML, LAZ, LAS, ZIP,'
-                                 'XML', 'CSV')
+                                 'XML, CSV')
 
 
 class Format(object):
