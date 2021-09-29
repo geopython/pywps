3c287e8d282be9828307f4b3a809dda7ada2306c
Carsten Ehbrecht <ehbrecht@dkrz.de>
2021-09-29 17:40:38 +0200

    ignore default when a value is required

--- pywps/inout/basic.py
+++ pywps/inout/basic.py
@@ -249,17 +249,25 @@ class IOHandler(object):
         value = value or getattr(self, '_default')
         value_type = value_type or getattr(self, '_default_type')
 
-        if value:
-            if value_type == SOURCE_TYPE.DATA:
-                self.data = value
-            elif value_type == SOURCE_TYPE.MEMORY:
-                raise NotImplementedError
-            elif value_type == SOURCE_TYPE.FILE:
-                self.file = value
-            elif value_type == SOURCE_TYPE.STREAM:
-                self.stream = value
-            elif value_type == SOURCE_TYPE.URL:
-                self.url = value
+        if self.min_occurs == 0:
+            # only set default when a value is optional
+            if value:
+                if value_type == SOURCE_TYPE.DATA:
+                    self.data = value
+                elif value_type == SOURCE_TYPE.MEMORY:
+                    raise NotImplementedError
+                elif value_type == SOURCE_TYPE.FILE:
+                    self.file = value
+                elif value_type == SOURCE_TYPE.STREAM:
+                    self.stream = value
+                elif value_type == SOURCE_TYPE.URL:
+                    self.url = value
+        else:
+            # when a value is requried the default value will be ignored
+            if value:
+                LOGGER.warning(
+                    "The given default value will not be used"
+                    " because is is required to provide a value.")
 
     def _build_file_name(self, href=''):
         """Return a file name for the local system."""
