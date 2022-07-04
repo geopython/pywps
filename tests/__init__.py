ec205152b762ddfbc197b90d36c5d8689f275232
Benoit Gschwind <gschwind@gnu-log.net>
2022-07-04 15:30:39 +0200

    Implement new tests/test_file_storage.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -29,6 +29,7 @@ from tests import test_processing
 from tests import test_assync
 from tests import test_grass_location
 from tests import test_storage_database
+from tests import test_storage_file
 from tests.validator import test_complexvalidators
 from tests.validator import test_literalvalidators
 
@@ -91,6 +92,7 @@ def load_tests(loader=None, tests=None, pattern=None):
         test_assync.load_tests(),
         test_grass_location.load_tests(),
         test_storage_database.load_tests(),
+        test_storage_file.load_tests(),
     ])
 
 
